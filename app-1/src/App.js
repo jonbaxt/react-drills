25131b90a221efb5dd0dc25e095d17b8c6d0a005
jonbaxt <jonbaxt@gmail.com>
2018-04-20 21:31:26 -0600

    Some Ref for App-1, App-2 No references

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+//Create a basic react app where you type in a text box and it shows up as text on the DOM.
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      messages: ''
+    }
+  }
+  update(newText) {
+    this.setState({ messages: newText });
+  }
+
   render() {
+    // let words = this.state.messages.map((word) => {
+      // return word;
+    // })
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div id='compBox'>
+          <input id='text-box' type='text' onChange={(val) => this.update(val.target.value)} />
+          <h2 >{this.state.messages}</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+// Create an app where there is an array of data on state that is then shown on the DOM as a list. 
+// The array of data can be as simple as an array of strings. 
+// The list can be as simple as a list of <h2> elements.
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      myList: ['Pigs', 'Turkeys', 'Goats']
+    }
+  }
+  
+  //Didn't need to refer to example code for this one.
   render() {
+    const headWord = <h2>This is my list! All Mine!</h2>
+    let listElements = this.state.myList.map( (item) => {
+     return <p> {item}  </p>
+    })
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <header>
+          {headWord}
+        </header>
+
+        <main>
+          <img id='left-image' src='https://http.cat/401' alt='https://http.cat/404' />
+          <span id='container'>  {listElements} </span>      
+          <img id='right-image'src='https://http.cat/305' alt='https://http.cat/415' />
+           </main>
       </div>
     );
   }
