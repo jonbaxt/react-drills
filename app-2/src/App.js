1169058287ee2ba08eee69f104e0aab94ac28f27
jonbaxt <jonbaxt@gmail.com>
2018-04-20 23:04:30 -0600

    Fast, didn't need to consult solution

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+//Create an app where there is an array of data on state that is then shown on the DOM as a list. 
+// The array of data can be as simple as an array of strings. 
+// The list can be as simple as a list of <h2> elements.
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      myStuff: ['Books', 'Candy', 'Calculator', 'T-Shirt', 'Toms Sandwich that he still didnt eat', 'Cookies', 'Milk', 'Root Beer']
+    }
+
+  }
+
   render() {
+  const createList = this.state.myStuff.map( (item) => {
+  return <p>{item}</p>
+  });
+    
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>My Stuff That I Have Right Now:</h1>
+        {createList}
       </div>
     );
   }
