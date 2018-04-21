6974a82e0d29ce75f2ee59cf34fc16fd5e16b9d0
jonbaxt <jonbaxt@gmail.com>
2018-04-20 20:39:12 -0600

    Finished try-1 first try

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
 import './App.css';
 
 // Create an app where there is an array of data on state that is then shown on the DOM as a list. 
@@ -7,31 +6,78 @@ import './App.css';
 // The array of data can be as simple as an array of strings. 
 // The list can be as simple as a list of <h2> elements.
 
+// Step 1: Clarify - Understand the question
+/*    -We need an array of data that is on the DOM, that already contains data presented as a list.
+      -The filter of the list is needed to to start filtering the list to look for elements on the state
+          that match the the first letters being typed into the text box.
+      - Array of strings is data
+      - List can be h2 elements.*/
+// Step 2: What data structures are needed
+/*    -Structures:
+        -Array of Different strings
+          -a map to pass the strings to the component in the browser.
+          -a filter to filter out the strings not containing the words need to go to the console.
+        -Another variable to catch information being typed to the text box
+        -h2 tags with ids for callbacks
+        
+*/
+// Step 3: Create sample data
+/*        myData : ['tortoise', 'sonic', 'tails', 'kit-kats', 'knuckles', 'hedgehogs', 'hair', 'dandilions']
+          currentWord: ''
+          modifiedArray: [] - to pass the variables that are only to be displayed when typing is sensed. */
+// Step 4: Solve Sample Data
+/*      myData ; ['tortoise', 'sonic', 'tails', 'kit-kats', 'knuckles', 'hedgehogs', 'hair', 'dandilions']
+            if('t' === currentWord)--> modifiedArray: ['tortoise, 'tails']
+            if('to' === currentWord)--> modifiedArray: ['tortoise']
+            if('d' === currentWord)--> modifiedArray: ['dandilions']
+            if('x' === currentWord)--> modifiedArray: []
+*/
+// Step 5: Function Signature
+/*  var myList = this.state.myData.map( (value, index) => {return list and <p>
+
+
+*/
+// Step 6: List code constructs
+/* */
+// Step 7: Pseudo Code
+/* */
+// Step 8: Solve
+
 class App extends Component {
   constructor(){
     super()
     this.state = {
-      myData : ['pink-flamingos', 'sonic', 'tails', 'knuckles', 'hedgehogs', 'dandilions'],
-      currentWord: ''
+      myData : ['tortoise', 'sonic', 'tails', 'kit-kats', 'knuckles', 'hedgehogs', 'hair', 'dandilions'],
+      currentWord: '',
     }
+    
   }
 
-  getCharacter(char){
+  updateList(char){
     this.setState({currentWord: char})
-
-
-
   }
 
   render() {
   
-    var myList = this.state.myData.map( (value, index) => {
-      return <p key={'item' + index}>Item {index+1}: {value}</p>
+/*
+  let foodsToDisplay = this.state.foods.filter( (element, index) => {
+      return element.includes( this.state.filterString );
+    }).map( (element, index) => {
+      return <h2 key={ index }>{ element }</h2>
+    })
+*/ 
+    
+    let myList = this.state.myData.filter( (val, ind) => {
+      return val.includes( this.state.currentWord);
+    }).map( (value, index) => {
+      return <p key={ index }>{value}</p>
     });
+/*Item {index+1}:*/
+
 
     return (
       <div className="App">
-        <input type='text' keyword='Search List' onChange={ () => this.getCharacter( (character) => character.target.value )} />
+        <input id='text-box' type='text' onChange={ (character) => this.updateList( character.target.value )} />
           {myList}
       </div>
     );
