80e7a71cee0fd9dc30be6df2a31e0e942f82e45d
jonbaxt <jonbaxt@gmail.com>
2018-04-23 18:18:08 -0600

    Some syntax errors after coding blind

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,20 +1,50 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Todo from './Todo';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      list: [],
+      newTask: ''
+    }
+    this.updateList = this.updateList.bind( this );
+  }
+
+  setTask(theTask) {
+    this.setState({
+      newTask: theTask
+    })
+  }
+
+  updateList(){
+    this.setState({
+      list: [...this.state.list, this.state.newTask],
+      newTask: ''
+    })
+  }
+
   render() {
+    let list = this.state.list.map( ( element, index ) => {
+      return (
+        <Todo key={ index } task={ element } />
+      )
+    })
+  
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1>My to-do list:</h1>
+        <input value={this.state.newTask}
+        placeholder='New task'
+        onChange={ (elem) => this.setTask(elem.target.value)} />
+        <button onClick={this.updateList}>Add</button>
+      <br />
+        { list }         
       </div>
-    );
+
+    )
   }
 }
 
