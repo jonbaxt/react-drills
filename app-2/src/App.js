21b25dde28a359d75ad1d86c952149c614a1a0a8
jonbaxt <jonbaxt@gmail.com>
2018-04-25 21:27:08 -0600

    Finished Quick

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,16 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+ constructor(){
+   super()
+   this.state = {
+     list: ['Dog', 'Cat', 'Wolf', 'Bunny', 'Parakeet', 'T-Rex']
+   }
+ }
+ 
+   render() {
+    let renderList = this.state.list.map( (petName, index ) => {
+      return <p key={index}>{`${index+1}. ${petName}`} </p>
+    })
+    
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <img src={`https://www.partnersforhealthypets.org/healthypetcheckup/assets/pets-fcb53b73523cd42be71be807ca0d6aaf.jpg`} className="App-logo" alt="logo" />
+          <h2>My Pet Shop</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <h3>Currently in Stock</h3>
+          {renderList}
       </div>
     );
   }
