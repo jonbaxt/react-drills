a906d2b016182409c3196a8c76901c540f08cfb2
jonbaxt <jonbaxt@gmail.com>
2018-04-25 21:18:10 -0600

    No reference at all, did more than required

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -3,16 +3,42 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      list: [],
+      input: ''
+    }
+  }
+  
+  handleTextBox(typing){
+    this.setState({input: typing});
+  }
+
+  saveInput(newInput){
+    this.setState({list: [...this.state.list, newInput],
+                  input: ''
+    })
+  }
+
   render() {
+    let displayList = this.state.list.map( (listItem, index) => {
+       return <p key={index}>{`Item ${index+1}: ${listItem}`}</p>
+    })
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <h2>Type Something</h2>
+          </div>
+            <input type='text'
+              placeholder='Type here'
+              value={this.state.input}
+              onChange={ (newElement) => this.handleTextBox(newElement.target.value)} />
+            <button onClick={() => this.saveInput(this.state.input)}>Submit</button>
+            <br />
+            { displayList }
       </div>
     );
   }
